672aff0db027dcd69536cce43201e2392b5bd453
shivaji <shivajithattikota@outlook.com>
2020-08-26 12:04:06 +0530

    Creating components and basic structuring

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test description',
+      'https://p0.pikist.com/photos/666/25/food-meat-recipe-power-pork-dishes.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test description',
+      'https://p0.pikist.com/photos/666/25/food-meat-recipe-power-pork-dishes.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
